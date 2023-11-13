c1099da532418612aef3928322c8611ac31e202a
ArfinHasib <arfinhasib21375@gmail.com>
2023-11-13 09:12:13 +0600

    initialization and section building

---
+++ src/components/Step.jsx
@@ -0,0 +1,12 @@
+const Step = ({ title }) => {
+   return (
+      <div className='flex flex-col gap-2.5 w-fit md:w-[310px] h-[165px] md:h-fit'>
+         <p className='text-dark-gray font-bold uppercase'>{title}</p>
+         <p className='text-light-gray'>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+         </p>
+      </div>
+   );
+};
+
+export default Step;
