042c89d45915dfc235f39304109640a09a813f37
ArfinHasib <arfinhasib21375@gmail.com>
2023-11-13 10:49:27 +0600

    Updating the service card

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,12 +1,12 @@
 const ServiceCard = ({ iconSrc, title }) => {
    return (
-      <div className='bg-white py-10 rounded-10 flex flex-col items-center shadow-lg max-w-[312px]'>
+      <div className='bg-white py-10 px-6 rounded-10 flex flex-col items-center shadow-lg max-w-[312px]'>
          <img src={iconSrc} alt={title} />
          <p className='uppercase text-dark-gray font-bold mb-2.5 mt-[18px] text-center'>
             {title}
          </p>
 
-         <p className='text-subtitle-gray text-gray p-2 text-center'>
+         <p className='text-subtitle-gray text-gray text-center'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente,
             distinctio.
          </p>
